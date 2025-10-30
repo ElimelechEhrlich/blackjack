63d8275c8b1ae1da9db34371cd941bc7ddda35c2
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-30 12:15:03 +0200

    edit ask_player_action function

--- core/player_io.py
+++ core/player_io.py
@@ -1,5 +1,6 @@
 def ask_player_action() -> str:
     choice = str(input('HIT OR STAND? H/S '))
+    choice = choice.upper()
     if choice != 'H' and choice != 'S':
         return ask_player_action()
     return choice
