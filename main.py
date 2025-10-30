47ad21bb5b657255837e0fe91408f96ff85d676d
Elimelech Ehrlich <ee525566@gmail.com>
2025-10-30 12:51:48 +0200

    fixed

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from core.deck import *
 from core.player_io import *
+from core.game_logik import *
 
 
 if __name__ == "__main__":
     deck = shuffle_by_suit(build_standard_deck())
+    player = {'hand':[]}
+    dealer = {'hand':[]}
+    run_full_game(deck,player,dealer)
